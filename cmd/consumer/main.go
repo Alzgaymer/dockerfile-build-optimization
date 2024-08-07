b955c9e4258b78ba2432815a8219d0340dfb85dc
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2024-08-07 22:56:42 +0300

    distributed projects

--- cmd/consumer/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("You successfully ran consumer!")
-}
---
+++ some-project-one/cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("You successfully ran server!\nP.S. From some-project-one")
+}
